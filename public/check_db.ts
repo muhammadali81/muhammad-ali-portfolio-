902b9865046e3ec21a0c3b4b000a7106b7d7f00c
Muhammad Ali <105591801+muhammadali81@users.noreply.github.com>
2026-08-30 13:56:06 -0700

    Delete public directory

--- public/check_db.ts
+++
@@ -1,8 +0,0 @@
-import { db } from './src/db';
-import { feedback } from './src/db/schema';
-async function check() {
-  const all = await db.select().from(feedback);
-  console.log('Feedback count:', all.length);
-  process.exit(0);
-}
-check();
